ff643b05d9c9229c8244ce492a1e809a2a1e6457
alexvirtech <alex@vir-tec.net>
2023-09-02 14:52:42 +0300

    first commit

---
+++ set_git.bat
@@ -0,0 +1,12 @@
+:: to run use .\set_git.bat
+@echo off
+::echo this repository is already initialized, insert :: at the beginning of the rows 3,4 in order to reinitialize
+::exit /b
+
+echo "# braude-datatable-purejs" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/alexvirtech/braude-datatable-jquery.git
+git push -u origin main
